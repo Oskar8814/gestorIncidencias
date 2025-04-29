a10cfc4f24f1bfb80c54b4c76de40842f760072d
DESKTOP-VCI486A\oscar <oscar_gs21@hotmail.com>
2025-04-29 13:33:06 +0200

    Añadir modal para mostrar imágenes de incidencias: implementar estructura del modal en las vistas y actualizar el script para incluir la descripción de la incidencia.

--- target/classes/static/js/app-modal.js
+++ target/classes/static/js/app-modal.js
@@ -5,23 +5,27 @@ modalImagen.addEventListener('show.bs.modal', function(event) {
     const enlace = event.relatedTarget; // Elemento que disparó el modal
 
     const rutaImagen = enlace.getAttribute('data-bs-imagen'); // Obtener la ruta de la imagen
+    const descripcionIncidencia = enlace.getAttribute('data-bs-descripcion'); // Obtener la descripción de la incidencia
     const tituloIncidencia = enlace.closest('tr').querySelector('td:nth-child(2)').textContent; // Obtener el título de la incidencia
 
     // Crear la imagen
     const imagen = document.createElement('IMG');
-    imagen.src = `img/${rutaImagen}.jpg`;
+    imagen.src = `${rutaImagen}`; // Asignar la ruta de la imagen
     imagen.classList.add('img-fluid');
     imagen.alt = 'Imagen incidencia';
 
     // Obtener elementos del modal
     const contenidoModal = document.querySelector('.modal-body');
     const tituloModal = document.querySelector('#tittleModal');
+    const descripcionModal = document.querySelector('#descripcionModal');
 
     // Limpiar el contenido anterior
     contenidoModal.textContent = '';
 
-    // Asignar el título y la imagen al modal
+    // Asignar el título y la descripcion al modal
     tituloModal.textContent = tituloIncidencia;
+    descripcionModal.textContent = descripcionIncidencia;
+    // Agregar la imagen al modal
     contenidoModal.appendChild(imagen);
 });
 
